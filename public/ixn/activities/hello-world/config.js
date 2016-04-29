b09a0438552d0189569a5ed85298c0899d239af2
zakariaeljaz <zakaria.eljaz@gmail.com>
2016-04-29 15:40:08 +0200

    a c

--- public/ixn/activities/hello-world/config.js
+++ public/ixn/activities/hello-world/config.js
@@ -1,7 +1,7 @@
 define([], function(){
     return {
-        "icon": "images/icon.png",
-        "iconSmall": "images/iconSmall.png",
+        "icon": "images/salesforce_icon.jpg",
+        "iconSmall": "images/salesforce_icon.jpg",
         "key": "jb-hello-world-activity-zakaria",
         "partnerApiObjectTypeId": "IXN.CustomActivity.REST",
         "lang": {
